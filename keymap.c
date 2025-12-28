44efc5c1e1eae311a0190fe8ddf4110aa7680bcb
boosja <mathias.iversen487@gmail.com>
2025-12-28 13:58:37 +0100

    Make it easier to access search-view in fedora

--- keymap.c
+++ keymap.c
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   // Linux layers
   [_LINUX_BASE] = LAYOUT_voyager(
-    NO_QUOT,        KC_1,          KC_2,     KC_3,     KC_4,     KC_5,                KC_6,   KC_7,     KC_8,       KC_9,      KC_0,          NO_BSLS,
+    KC_LGUI,       KC_1,          KC_2,     KC_3,     KC_4,     KC_5,                KC_6,   KC_7,     KC_8,       KC_9,      KC_0,          NO_BSLS,
     KC_TAB,         KC_Q,          KC_W,     KC_E,     KC_R,     KC_T,                KC_Y,   KC_U,     KC_I,       KC_O,      KC_P,          NO_SLSH,
     KC_BSPC,        LCTL_A,        LALT_S,   LSFT_D,   LCTL_F,   KC_G,                KC_H,   RCTL_J,   RSFT_K,     RALT_L,    DUAL_FUNC_1,   NO_AT,
     KC_ESCAPE,      KC_Z,          KC_X,     KC_C,     KC_V,     KC_B,                KC_N,   KC_M,     KC_COMM,    KC_DOT,    NO_MINS,       KC_HYPR,
