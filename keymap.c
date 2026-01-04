120f95073aa0a2529ff9718561e6981fc98f4acd
boosja <mathias.iversen487@gmail.com>
2026-01-04 13:54:47 +0100

    But keep the dead corpses too

--- keymap.c
+++ keymap.c
@@ -123,10 +123,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                 LSUPER_SPACE,  MO(_LNX_MISC),            OSL(_LNX_SYMBS), RSUPER_ENTER
   ),
   [_LNX_SYMBS] = LAYOUT_voyager(
-    _______,       KC_F1,         KC_F2,         KC_F3,         KC_F4,         KC_F5,                    KC_F6,         KC_F7,         KC_F8,         KC_F9,         KC_F10,        KC_F11,
-    _______,       NO_EXLM,       NO_DLR,        _______,       NO_BTCK_LIVE,  MY_AA,                    NO_AE,         NO_PLUS,       NO_MINS,       NO_EQL,        _______,       KC_F12,
+    NO_GRV,        KC_F1,         KC_F2,         KC_F3,         KC_F4,         KC_F5,                    KC_F6,         KC_F7,         KC_F8,         KC_F9,         KC_F10,        KC_F11,
+    NO_CIRC,       NO_EXLM,       NO_DLR,        _______,       NO_BTCK_LIVE,  MY_AA,                    NO_AE,         NO_PLUS,       NO_MINS,       NO_EQL,        _______,       KC_F12,
     _______,       KC_LCTL,       KC_LALT,       KC_LSFT,       LCTL_HASH,     MY_OE,                    NO_QUES,       RCTL_QUOT,     RSFT_DQUO,     RALT_SLSH,     RGUI_ASTR,     _______,
-    _______,       _______,       NO_CIRC_LIVE,  NO_TILD_LIVE,  NO_PERC,       _______,                  NO_AMPR,       NO_LABK,       NO_RABK,       NO_BSLS,       _______,       _______,
+    NO_TILD,       _______,       NO_CIRC_LIVE,  NO_TILD_LIVE,  NO_PERC,       _______,                  NO_AMPR,       NO_LABK,       NO_RABK,       NO_BSLS,       _______,       _______,
                                                                 TO(_LNX_BASE), _______,                  TO(_LNX_MISC), _______
   ),
   [_LNX_MISC] = LAYOUT_voyager(
